ed6b6794eab3e87011b0e047af62c46400d2ca40
Bartmor <bartmor.21@gmail.com>
2024-06-29 17:37:18 +0200

    fin

--- Data clustering/Data clustering.cpp
+++ Data clustering/Data clustering.cpp
@@ -238,7 +238,126 @@ private:
     matrixU next_;
     std::vector<data> clusters_;
 };
+class fuzzyClustering : public data
+{
+    using matrixU = std::vector<std::vector<double>>;
+    using matrixP = std::vector<point>;
+    using cluster = std::vector<point>;
+public:
+    /// <summary>
+    /// constructor to calculate crisp clustering it is doing all work for you...
+    /// </summary>
+    /// <param name="rowData">set of points</param>
+    /// <param name="numberOfClusters">number of clusters</param>
+    /// <param name="calculationDepth">max number of repetition of gruping</param>
+    fuzzyClustering(const std::vector<point> &rowData, const size_t &numberOfClusters, const double &fuzzyfyingFactor, const size_t &calculationDepth)
+        : data(rowData)
+        , numberOfClusters_(numberOfClusters)
+        , calculationDepth_(calculationDepth)
+        , fuzzyfyingFactor_(fuzzyfyingFactor)
+        , previous_(initU())
+    {
+        for (size_t depthIndex(0); depthIndex < calculationDepth_; ++depthIndex)
+        {
+            next_.resize(size(), std::vector<double>(numberOfClusters_, 0));
+            matrixP currentPrototypes = calculePrototypes(previous_);
+            std::vector<double> distances;
+            for (size_t pointIndex(0); pointIndex < size(); ++pointIndex)
+            {
 
+                for (size_t assignedClusterIndex(0); assignedClusterIndex < numberOfClusters_; ++assignedClusterIndex)
+                {
+                    double assignedClusterDistance = get().at(pointIndex).distance(currentPrototypes.at(assignedClusterIndex));
+                    assignedClusterDistance = pow(assignedClusterDistance, (-2.) / (fuzzyfyingFactor_ - 1));
+                    double totalDistances = 0;
+                    for (size_t clusterIndex(0); clusterIndex < numberOfClusters_; ++clusterIndex)
+                    {
+                        double tempDistance = get().at(pointIndex).distance(currentPrototypes.at(clusterIndex));
+                        tempDistance = pow(tempDistance, (-2.) / (fuzzyfyingFactor_ - 1));
+                        totalDistances += tempDistance;
+                    }
+                    next_.at(pointIndex).at(assignedClusterIndex) = assignedClusterDistance / totalDistances;
+                }
+
+            }
+            previous_ = next_;
+            next_.clear();
+            for (auto i : previous_)
+            {
+                for (auto j : i)
+                    std::cout << j << " ";
+                std::cout << std::endl;
+            }
+
+        }
+        data rawPoints(get());
+        cluster tempCluster;
+        for (size_t clusterIndex(0); clusterIndex < numberOfClusters_; ++clusterIndex)
+        {
+            for (size_t pointIndex(0); pointIndex < size(); ++pointIndex)
+            {
+                if (previous_.at(pointIndex).at(clusterIndex) >= .5)
+                {
+                    tempCluster.push_back(get().at(pointIndex));
+                }
+            }
+            clusters_.push_back(tempCluster);
+            tempCluster.clear();
+        }
+    }
+    std::vector<data>& getClusters()
+    {
+        return clusters_;
+    }
+private:
+    matrixU initU()
+    {
+        matrixU temp;
+        temp.resize(size(), std::vector<double>(numberOfClusters_, 0));
+
+        size_t counter(0);
+        for (size_t i(0); i < temp.size(); ++i)
+        {
+
+            for (size_t j(0); j < numberOfClusters_; ++j)
+            {
+                if (j == counter)
+                {
+                    temp.at(i).at(j) = 1;
+                }
+            }
+            ++counter;
+            if (counter >= numberOfClusters_)
+            {
+                counter = 0;
+            }
+        }
+        return temp;
+    }
+    matrixP calculePrototypes(const matrixU& currentMatrixU)
+    {
+        matrixP temp;
+        temp.resize(numberOfClusters_, point(0, 0));
+        for (size_t i(0); i < temp.size(); ++i)
+        {
+            point tempPoint(0, 0);
+            double tempValue(0);
+            for (size_t j(0); j < size(); ++j)
+            {
+                tempValue += currentMatrixU.at(j).at(i);
+                tempPoint += get().at(j) * currentMatrixU.at(j).at(i);
+            }
+            temp.at(i) = tempPoint / tempValue;
+        }
+        return temp;
+    }
+    const size_t numberOfClusters_;
+    const size_t calculationDepth_;
+    const double fuzzyfyingFactor_;
+    matrixU previous_;
+    matrixU next_;
+    std::vector<data> clusters_;
+};
 static data dRead(fs::path& path)
 {
     std::fstream inputFile(path);
@@ -261,7 +380,19 @@ static data dRead(fs::path& path)
     }
     return data(formatedData);
 }
-
+void ploting(data frist, std::vector<data> second)
+{
+    matplot::figure();
+    matplot::scatter(frist.getX(), frist.getY());
+    matplot::figure();
+    matplot::hold(matplot::on);
+    for (size_t clusterIndex(0); clusterIndex < 2; ++clusterIndex)
+    {
+        data dataToPrint = second.at(clusterIndex);
+        matplot::scatter(dataToPrint.getX(), dataToPrint.getY());
+    }
+    matplot::hold(matplot::off);
+}
 int main()
 {
     fs::path filePathDC = "C:\\Users\\Studia\\source\\repos\\Data clustering\\Data clustering\\DC-Data4.txt";
@@ -270,30 +401,15 @@ int main()
     const data DC(dRead(filePathDC));
     const data DCN(dRead(filePathDCN));
     const data testD(dRead(testPath));
-    //crispClustering testSet(testD.get(), 2, 3);
-    crispClustering dataSetDC(DC.get(), 2, 2);
 
-    crispClustering dataSetDCN(DCN.get(), 2, 5);
-    //matplot::figure();
-    //matplot::scatter(DC.getX(), DC.getY());
-    //matplot::figure();
-    //matplot::hold(matplot::on);
-    //for (size_t clusterIndex(0); clusterIndex < 2; ++clusterIndex)
-    //{
-    //    matplot::scatter(dataSetDC.getClusters().at(clusterIndex).getX(), dataSetDC.getClusters().at(clusterIndex).getY());
-    //}
-    matplot::figure();
-    matplot::scatter(DCN.getX(), DCN.getY());
-    matplot::figure();
-    matplot::hold(matplot::on);
-    for (size_t clusterIndex(0); clusterIndex < 2; ++clusterIndex)
-    {
-        data dataToPrint = dataSetDCN.getClusters().at(clusterIndex);
-        matplot::scatter(dataSetDCN.getClusters().at(clusterIndex).getX(), dataSetDCN.getClusters().at(clusterIndex).getY());
-    }
-    //matplot::scatter(DC.getX(), DC.getY());
-    //matplot::scatter(DCN.getX(), DCN.getY());
+    crispClustering dataSetDCCrisp(DC.get(), 2, 2);
+    crispClustering dataSetDCNCrisp(DCN.get(), 2, 5);
+    fuzzyClustering dataSetDCFuzzy(DC.get(), 2, 2, 2);
+    fuzzyClustering dataSetDCNFuzzy(DCN.get(), 2, 2, 5);
 
-
+    ploting(DC, dataSetDCCrisp.getClusters());
+    ploting(DCN, dataSetDCNCrisp.getClusters());
+    ploting(DC, dataSetDCFuzzy.getClusters());
+    ploting(DCN, dataSetDCNFuzzy.getClusters());
     matplot::show();
 }
