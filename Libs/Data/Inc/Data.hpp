30e5ad52b70355097d440b84b9aadb2c2d627730
Bartmor <bartmor.21@gmail.com>
2025-04-10 18:28:37 +0200

    nodiscard adjustment

--- Libs/Data/Inc/Data.hpp
+++ Libs/Data/Inc/Data.hpp
@@ -30,13 +30,13 @@ public:
 
     [[nodiscard("Data::getDataCenter")]] Point getDataCenter() const;
     [[nodiscard("Data::size")]] size_t size() const;
-    bool empty() const;
-    bool operator==(const Data& other) const;
+    [[nodiscard("Data::empty")]] bool empty() const;
+    [[nodiscard("Data::operator==")]] bool operator==(const Data& other) const;
     void print() const;
-    auto begin();
-    auto cbegin() const;
-    auto end();
-    auto cend() const;
+    [[nodiscard("Data::begin")]] auto begin();
+    [[nodiscard("Data::cbegin")]] auto cbegin() const;
+    [[nodiscard("Data::end")]] auto end();
+    [[nodiscard("Data::cend")]] auto cend() const;
     void push_back(const Point& point);
     void push_back(const Data& data);
     void pop_back();
